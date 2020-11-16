4f779b621559c2987173a37281e260aa20380643
ivolff <ivanvolff@gmail.com>
2020-11-16 22:36:21 +0300

    Tokens remuve

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,39 @@
 <template>
   <div>
+    <Token
+        v-for="tok in toks"
+        :key="tok.id"
+        :tkn="tok"
+    />
     <Map />
-    <Tokens />
   </div>
 </template>
 
 <script>
 import Map from '@/components/Map'
 import Token from '@/components/Token'
-import Tokens from '@/components/Tokens'
 
 export default {
   name: 'home',
   components: {
-    Map, Token, Tokens
+    Map, Token
+  },
+  data: () => ({
+    count: 3,
+    toks: [
+      {title: "name1", id: 1, path: require('../assets/tkn/tmp1.png')},
+      {title: "name2", id: 2, path: require('../assets/tkn/tmp2.png')}
+    ]
+  }),
+  methods: {
+    addToken: function() {
+      this.toks.push({
+        title: "name3",
+        id: this.count++,
+        path: require('../assets/tkn/tmp2.png')
+      })
+      return;
+    }
   },
 }
 </script>
